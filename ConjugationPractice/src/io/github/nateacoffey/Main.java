b490b7b7428454a8e675524e8e977499d813b1d2
nathan <nathan@pop-os.localdomain>
2020-10-02 11:52:37 -0700

    Added favicon

--- ConjugationPractice/src/io/github/nateacoffey/Main.java
+++ ConjugationPractice/src/io/github/nateacoffey/Main.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 
@@ -17,6 +18,7 @@ public class Main extends Application {
 			
 			Parent root = FXMLLoader.load(getClass().getResource("LanguageSelector/LanguageSelector.fxml"));
 			Scene scene = new Scene(root, 800, 450);
+			primaryStage.getIcons().add(new Image("file:src/io/github/nateacoffey/favicon.png"));
 			primaryStage.setTitle("Conjugation Practice");
 			primaryStage.setScene(scene);
 			primaryStage.show();
